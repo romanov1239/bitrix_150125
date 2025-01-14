675b59d40a4b3e8da73475a7434da8776a55617c
Ваше Имя <_email@example.com>
2025-01-14 15:29:26 +0300

    новости_1

--- index.php
+++ index.php
@@ -267,6 +267,63 @@ $APPLICATION->SetTitle("1С-Битрикс: Управление сайтом");
 			<div class="standart_width">
 				<div class="title">Новости</div>
 				<div class="news_list">
+                    <?$APPLICATION->IncludeComponent(
+                        "bitrix:news.list",
+                        "news_index",
+                        Array(
+                            "ACTIVE_DATE_FORMAT" => "j F Y",
+                            "ADD_SECTIONS_CHAIN" => "Y",
+                            "AJAX_MODE" => "N",
+                            "AJAX_OPTION_ADDITIONAL" => "",
+                            "AJAX_OPTION_HISTORY" => "N",
+                            "AJAX_OPTION_JUMP" => "N",
+                            "AJAX_OPTION_STYLE" => "Y",
+                            "CACHE_FILTER" => "N",
+                            "CACHE_GROUPS" => "Y",
+                            "CACHE_TIME" => "36000000",
+                            "CACHE_TYPE" => "A",
+                            "CHECK_DATES" => "Y",
+                            "DETAIL_URL" => "",
+                            "DISPLAY_BOTTOM_PAGER" => "Y",
+                            "DISPLAY_DATE" => "Y",
+                            "DISPLAY_NAME" => "Y",
+                            "DISPLAY_PICTURE" => "Y",
+                            "DISPLAY_PREVIEW_TEXT" => "Y",
+                            "DISPLAY_TOP_PAGER" => "N",
+                            "FIELD_CODE" => array("SHOW_COUNTER",""),
+                            "FILTER_NAME" => "",
+                            "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                            "IBLOCK_ID" => "3",
+                            "IBLOCK_TYPE" => "info",
+                            "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+                            "INCLUDE_SUBSECTIONS" => "Y",
+                            "MESSAGE_404" => "",
+                            "NEWS_COUNT" => "3",
+                            "PAGER_BASE_LINK_ENABLE" => "N",
+                            "PAGER_DESC_NUMBERING" => "N",
+                            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                            "PAGER_SHOW_ALL" => "N",
+                            "PAGER_SHOW_ALWAYS" => "N",
+                            "PAGER_TEMPLATE" => ".default",
+                            "PAGER_TITLE" => "Новости",
+                            "PARENT_SECTION" => "",
+                            "PARENT_SECTION_CODE" => "",
+                            "PREVIEW_TRUNCATE_LEN" => "",
+                            "PROPERTY_CODE" => array("",""),
+                            "SET_BROWSER_TITLE" => "Y",
+                            "SET_LAST_MODIFIED" => "N",
+                            "SET_META_DESCRIPTION" => "Y",
+                            "SET_META_KEYWORDS" => "Y",
+                            "SET_STATUS_404" => "N",
+                            "SET_TITLE" => "Y",
+                            "SHOW_404" => "N",
+                            "SORT_BY1" => "ACTIVE_FROM",
+                            "SORT_BY2" => "SORT",
+                            "SORT_ORDER1" => "DESC",
+                            "SORT_ORDER2" => "ASC",
+                            "STRICT_SECTION_CHECK" => "N"
+                        )
+                    );?>
 					<div class="news_el">
 						<div class="news_el_img"><a href=""><img src="<?=SITE_TEMPLATE_PATH?>/img/news1.png" alt=""/></a></div>
 						<div class="news_el_cont">
