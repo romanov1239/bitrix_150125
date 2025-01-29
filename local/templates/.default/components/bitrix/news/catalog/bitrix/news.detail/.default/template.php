6b0412598c6109f33c2dd35686cf2a91a0783d7d
Ваше Имя <_email@example.com>
2025-01-29 10:50:02 +0300

    карточка товара 3

--- local/templates/.default/components/bitrix/news/catalog/bitrix/news.detail/.default/template.php
+++ local/templates/.default/components/bitrix/news/catalog/bitrix/news.detail/.default/template.php
@@ -44,38 +44,57 @@ $arProps=$arResult['PROPERTIES'];
         <div class="detail_info_top">
             <h1><?= $arResult["NAME"];?></h1>
             <div class="cat_el_star">
-                <img src="./img/star_orange.png" alt=""/>
-                <img src="./img/star_orange.png" alt=""/>
-                <img src="./img/star_orange.png" alt=""/>
-                <img src="./img/star_orange.png" alt=""/>
-                <img src="./img/star_gray.png" alt=""/>
+                <?for($i=1;$i<=5;$i++):?>
+                    <?if($arProps['RATING']['VALUE']>=$i):?>
+                        <img src="<?=SITE_TEMPLATE_PATH?>/img/star_orange.png" alt=""/>
+                    <?else:?>
+
+
+                        <img src="<?=SITE_TEMPLATE_PATH?>/img/star_gray.png" alt=""/>
+                    <?endif;?>
+                <?endfor;?>
             </div>
             <div class="det_price_status_bl">
                 <div class="det_price_bl">
-                    <div class="det_price">450 руб.</div>
-                    <div class="cat_old_price">
-                        <div class="price">500 руб.</div>
-                        <div class="sale">-10%</div>
-                    </div>
+                    <?if(!$arProps['DISCOUNT']['VALUE']):?>
+                        <div class="det_price"><?=$arProps['PRICE']['VALUE']?>руб.</div>
+                    <?else:?>
+                        <div class="det_price"><?=$price_new=$arProps['PRICE']['VALUE']-($arProps['PRICE']['VALUE']*$arProps['DISCOUNT']['VALUE']/100)?></div>
+                        <div class="cat_old_price">
+                            <div class="price"><?=$arProps['PRICE']['VALUE']?>руб.</div>
+                            <div class="sale">-<?=$arProps['DISCOUNT']['VALUE']?>%</div>
+                        </div>
+                    <?endif;?>
                     <a class="favor_bt active" href="">В избранное</a>
                 </div>
                 <div class="det_status_bl">
+                    <?if($arProps['COUNT']['VALUE']>1):?>
                     <div class="det_status_el green">В наличии</div>
+                    <?elseif ($arProps['COUNT']['VALUE']==1):?>
                     <div class="det_status_el red">Осталось мало</div>
+                    <?else:?>
+                    <div class="det_status_el red">Под заказ</div>
+                    <?endif;?>
+
+
                     <div class="det_status_el blue">Бесплатная доставка</div>
                 </div>
             </div>
         </div>
         <div class="detail_info_bottom">
-            <div class="detail_option_list">
-                <div class="detail_option_el">Автор:  <a href="">Сергей Серый</a></div>
-                <div class="detail_option_el">Год издания:  2018</div>
-                <div class="detail_option_el">Издательство:  <a href="">Первый</a>, <a href="">Второй</a></div>
-                <div class="detail_option_el">Количество страниц:  123</div>
-                <div class="detail_option_el">Артикул:  c123456</div>
-                <div class="detail_option_el">Переплет:  Твердый (7-инт)</div>
-                <div class="detail_option_el">Возрастное ограничение:  6+</div>
-                <div class="detail_option_el">Формат:  170x217 мм</div>
+            <div class="detail_option_list"><?
+                foreach($arResult["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
+                <div class="detail_option_el">
+
+                <?=$arProperty["NAME"]?>:&nbsp;
+                <?if(is_array($arProperty["DISPLAY_VALUE"])):?>
+                <?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
+                <?else:?>
+                <?=$arProperty["DISPLAY_VALUE"];?>
+                <?endif?>
+                </div>
+                <?endforeach;?>
+
             </div>
         </div>
     </div>
